dac141d0fb632976fb62b5952739c03d9269bcb9
Dr.NP <conan.np@gmail.com>
2025-01-08 18:58:14 +0800

    first commit

---
+++ defs/general.go
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) HereweTech, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+/**
+ * @file general.go
+ * @package defs
+ * @author Dr.NP <conan.np@gmail.com>
+ * @since 01/04/2025
+ */
+
+package defs
+
+const (
+	AppName = "SmartRigger"
+)
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
