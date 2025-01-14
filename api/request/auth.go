b5956f80eef54816235fc39a8253ea7a457c324a
Dr.NP <conan.np@gmail.com>
2025-01-15 01:57:10 +0800

    feat : api request & response

---
+++ api/request/auth.go
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) HereweTech, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+/**
+ * @file auth.go
+ * @package request
+ * @author Dr.NP <conan.np@gmail.com>
+ * @since 01/14/2025
+ */
+
+package request
+
+type PostAuthLogin struct {
+	Username string `json:"username" xml:"username" form:"username"`
+	Password string `json:"password" xml:"password" form:"password"`
+}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
---
+++ api/response/auth.go
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) HereweTech, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+/**
+ * @file auth.go
+ * @package response
+ * @author Dr.NP <conan.np@gmail.com>
+ * @since 01/14/2025
+ */
+
+package response
+
+type PostAuthLogin struct{}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
