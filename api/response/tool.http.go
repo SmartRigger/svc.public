09ab455c23bb2e51f85bf3ea0c7de908a488a980
medtrib <medtrib@gmail.com>
2025-03-18 11:41:55 +0800

    增加一个字段

--- api/response/tool.http.go
+++ api/response/tool.http.go
@@ -25,5 +25,6 @@ type ApiHTTPGetTool struct {
 	CreatedAt                string `json:"created_at" yaml:"created_at" xml:"created_at"`
 	DailyStartupVerification bool   `json:"daily_startup_verification" yaml:"daily_startup_verification" xml:"daily_startup_verification"`
 	SelfMutualInspection     bool   `json:"self_mutual_inspection" yaml:"self_mutual_inspection" xml:"self_mutual_inspection"`
+	InspectionPersonnel      string `json:"inspection_personnel" yaml:"inspection_personnel" xml:"inspection_personnel"`
 	Detail                   string `json:"detail" yaml:"detail" xml:"detail"`
 }
