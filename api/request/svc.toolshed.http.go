559b1806e0222052d9c357c93012d3aca1312c81
Dr.NP <conan.np@gmail.com>
2025-07-15 01:21:16 +0800

    feat : svc.toolshed

---
+++ api/request/svc.toolshed.http.go
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) HereweTech, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+/**
+ * @file svc.toolshed.http.go
+ * @package request
+ * @author Dr.NP <conan.np@gmail.com>
+ * @since 07/13/2025
+ */
+
+package request
+
+type ToolshedHTTPEssential struct {
+	Name string `json:"name" xml:"name" form:"name"`
+}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
